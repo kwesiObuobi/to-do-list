dcf56d51a8f63b8fa44615a273b6b02334601470
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-03-28 10:38:52 +0000

    Add javascript code for dynamic loading of list items

--- src/index.js
+++ src/index.js
@@ -18,4 +18,37 @@ const tasks = [
   },
 ];
 
-tasks.sort((a, b) => a.index - b.index);
+const toDoBox = document.querySelector('.todo-box');
+
+tasks
+  .sort((a, b) => a.index - b.index)
+  .forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+
+    const listCheckAndName = document.createElement('div');
+    listCheckAndName.classList.add('list-check-and-name');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const spn = document.createElement('span');
+    spn.innerHTML = task.description;
+
+    const listElipsesBox = document.createElement('div');
+    listElipsesBox.classList.add('list-ellipses-box');
+    listElipsesBox.innerHTML = '<i class="fa-solid fa-ellipsis-vertical">';
+
+    listCheckAndName.appendChild(checkbox);
+    listCheckAndName.appendChild(spn);
+
+    listItem.appendChild(listCheckAndName);
+    listItem.appendChild(listElipsesBox);
+
+    toDoBox.appendChild(listItem);
+  });
+
+const clearLi = document.createElement('li');
+clearLi.classList.add('clear-li');
+clearLi.innerHTML = 'Clear all completed';
+toDoBox.appendChild(clearLi);
