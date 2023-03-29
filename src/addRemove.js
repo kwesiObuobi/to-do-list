944d1250c517d2592cdc133c1e080b15542a0fef
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-03-29 08:22:31 +0000

    Add code to change the ellipses to delete button when the input field is in focuse

--- src/addRemove.js
+++ src/addRemove.js
@@ -1,3 +1,21 @@
-const add = (task) => {
-  localStorage.setItem();
+import todolist from './todoList.js';
+
+export const add = (task) => {
+  todolist.push({
+    description: task.value,
+    completed: false,
+    index: todolist.length + 1,
+  });
+
+  localStorage.setItem('todolist', JSON.stringify(todolist));
+};
+
+export const hey = (task) => {
+  todolist.push({
+    description: task.value,
+    completed: false,
+    index: todolist.length,
+  });
+
+  localStorage.setItem('todolist', JSON.stringify(todolist));
 };
--- src/index.js
+++ src/index.js
@@ -1,55 +1,109 @@
 import './style.css';
+import todoList from './todoList.js';
+import { add } from './addRemove.js';
 
-const tasks = [
-  // {
-  //   description: 'Wash Planes',
-  //   completed: true,
-  //   index: 1,
-  // },
-  // {
-  //   description: 'Wash dishes',
-  //   completed: false,
-  //   index: 3,
-  // },
-  // {
-  //   description: 'Wash cars',
-  //   completed: true,
-  //   index: 2,
-  // },
-];
+const tasks = todoList;
 
 const toDoBox = document.querySelector('.todo-box');
 
-tasks
-  .sort((a, b) => a.index - b.index)
-  .forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-item');
+const renderTasks = () => {
+  toDoBox.innerHTML = '';
+  // todoIntro();
+  const headerLi = document.createElement('li');
+  headerLi.classList.add('header-li');
+  headerLi.innerHTML = 'Today\'s To Do <i class="fa-solid fa-rotate"></i>';
 
-    const listCheckAndName = document.createElement('div');
-    listCheckAndName.classList.add('list-check-and-name');
+  const addToList = document.createElement('li');
+  addToList.classList.add('add-to-list');
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
+  const form = document.createElement('form');
+  form.classList.add('add-list-form');
 
-    const itemValInput = document.createElement('input');
-    itemValInput.classList.add('list-item-value');
-    itemValInput.value = task.description;
+  const newItem = document.createElement('input');
+  newItem.id = 'new-item';
+  newItem.type = 'text';
+  newItem.placeholder = 'Add to your list';
+  newItem.setAttribute('required', '');
 
-    const listElipsesBox = document.createElement('div');
-    listElipsesBox.classList.add('list-ellipses-box');
-    listElipsesBox.innerHTML = '<i class="fa-solid fa-ellipsis-vertical">';
+  const submitBtn = document.createElement('button');
+  submitBtn.classList.add('submit-btn');
+  submitBtn.type = 'submit';
+  submitBtn.innerHTML = '<i class="fa-solid fa-plus"></i>';
 
-    listCheckAndName.appendChild(checkbox);
-    listCheckAndName.appendChild(itemValInput);
+  toDoBox.appendChild(headerLi);
+  toDoBox.appendChild(addToList);
+  addToList.appendChild(form);
+  form.appendChild(newItem);
+  form.appendChild(submitBtn);
 
-    listItem.appendChild(listCheckAndName);
-    listItem.appendChild(listElipsesBox);
-
-    toDoBox.appendChild(listItem);
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    add(newItem);
+    newItem.value = '';
+    renderTasks();
   });
+
+  tasks
+    .sort((a, b) => a.index - b.index)
+    .forEach((task) => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('list-item');
+
+      const listCheckAndName = document.createElement('div');
+      listCheckAndName.classList.add('list-check-and-name');
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+
+      const itemValInput = document.createElement('input');
+      itemValInput.classList.add('list-item-value');
+      itemValInput.value = task.description;
+
+      const listElipsesBox = document.createElement('div');
+      listElipsesBox.classList.add('list-ellipses-box');
+      listElipsesBox.innerHTML = '<i class="fa-solid fa-ellipsis-vertical">';
+
+      // Change ellipses to deleteBtn when the input field is in focus
+      itemValInput.addEventListener('focus', () => {
+        listElipsesBox.classList.add('del-btn');
+        listElipsesBox.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+      });
+
+      itemValInput.addEventListener('blur', () => {
+        listElipsesBox.classList.remove('del-btn');
+        listElipsesBox.innerHTML = '<i class="fa-solid fa-ellipsis-vertical">';
+      });
+      //
+
+      listCheckAndName.appendChild(checkbox);
+      listCheckAndName.appendChild(itemValInput);
+
+      listItem.appendChild(listCheckAndName);
+      listItem.appendChild(listElipsesBox);
+
+      toDoBox.appendChild(listItem);
+    });
+};
+renderTasks();
 
 const clearLi = document.createElement('li');
 clearLi.classList.add('clear-li');
 clearLi.innerHTML = 'Clear all completed';
 toDoBox.appendChild(clearLi);
+
+// const form = document.querySelector('form');
+// const input = document.getElementById('new-item');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   add(input);
+//   input.value = '';
+//   renderTasks();
+// });
+
+// const submitBtn = document.querySelector('.submit-btn');
+// submitBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   add(value);
+//   value.value = '';
+//   renderTasks();
+// });
